2ae61d84f742af67a44fe853ac5f71e6af80d312
Paul Maier <pauljustus279@gmail.com>
2022-04-06 17:46:49 +0200

    Migrated

--- prisma/migrations/20220406154554_create_state_and_district_model_and_relationship/migration.sql
+++ prisma/migrations/20220406154554_create_state_and_district_model_and_relationship/migration.sql
@@ -20,5 +20,8 @@ CREATE UNIQUE INDEX "State_name_key" ON "State"("name");
 -- CreateIndex
 CREATE UNIQUE INDEX "District_ags_key" ON "District"("ags");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "District_name_stateAgsPrefix_key" ON "District"("name", "stateAgsPrefix");
+
 -- AddForeignKey
 ALTER TABLE "District" ADD CONSTRAINT "District_stateAgsPrefix_fkey" FOREIGN KEY ("stateAgsPrefix") REFERENCES "State"("agsPrefix") ON DELETE RESTRICT ON UPDATE CASCADE;
